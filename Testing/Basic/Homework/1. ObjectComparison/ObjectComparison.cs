ee7b3d9109f299ece053120cd214500e76ef171f
dm7672 <dmitrsem10@gmail.com>
2025-10-30 15:48:14 +0500

    Переделал задание создание объектов

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -15,28 +16,38 @@ public class ObjectComparison
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
-        // Перепишите код на использование Fluent Assertions.
-        actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
-            options.Excluding(person => person.Id)
-                   .Excluding(person => person.Parent.Id)
-            );
-        // Недостатки подхода CustomEquality по сравнению с решением выше в том что
-        // 1. Используется проверка на истинность AreEqual, в случае ошибки покажет только то что AreEqual вернуло False
-        // 2. Кастомный Ассерт ниже нерасширяем, при попытке добавить новое поле в класс придётся переписывать его
-        // 3. Читаемость гораздо хуже, в ассерте могла быть пропущена проверка поля и внешне он выглядел бы также,
-        // а значит чтобы понять что пытаемся проверить пришлось бы читать весь код
-        // 4. Код выше короче
+        actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
+                .Excluding((IMemberInfo memberInfo) => memberInfo.Name == nameof(Person.Id) 
+                                                       && memberInfo.DeclaringType == typeof(Person))
+                .IgnoringCyclicReferences()
+                .AllowingInfiniteRecursion());
+        // Решение через FluidAssertions 
+        // 1. Лучше читается, сразу видно что assert проверяет на полное соответствие всех полей класса кроме всех Id у класса Person
+        // 2. Теперь поддерживает лёгкое расширение, если добавим новое поле которое требует проверки то не нужно будет переписывать assert,
+        // если добавим поле которое не требует проверки то необходимо будет добавить его имя в Excluding()
+        // 3. Поддерживает длинные цепочки родителей за счёт AllowingInfiniteRecursion(),
+        // иначе на 10(по дефолту в FluentAssertions) рекурсивном вызове в глубину проверка бы остановилась
+        // 4. Останавливается когда обнаруживает что проверка поля производилась раньше благодаря IgnoringCyclicReferences(),
+        // таким образом не проваливается в бесконечную рекурсию
+        // 5. При ошибке действительно показывает какое поле не совпало
+
+
     }
 
     [Test]
     [Description("Альтернативное решение. Какие у него недостатки?")]
     public void CheckCurrentTsar_WithCustomEquality()
     {
+        
         var actualTsar = TsarRegistry.GetCurrentTsar();
+
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
-
         // Какие недостатки у такого подхода? 
+        // 1. Он нерасширяем, при добавлении нового поля придётся переписывать AreEqual
+        // 2. Слабая читаемость, если в методе AreEqual допущена ошибка нужно смотреть на каждое поле которое он сравнивает чтобы найти ошибку
+        // 3. Возможна бесконечная рекурсия если parent образует цикл
+        // 4. При ошибке не показывает какие поля не совпадают, только что AreEqual вернуло False
         ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
     }
 
